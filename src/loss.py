8c74c4c4c4e689faf63a58a41ca70b4d62cd9064
jihoon <jh921204@naver.com>
2023-06-13 14:55:00 +0900

    docs.add ELBO_loss comment

--- src/loss.py
+++ src/loss.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 
 def ELBO_loss(recon_x, x, mu, std):
     """
-    BCE
-    KLD
+    BCE: 입력값 x와 decoder를 거쳐 생성된 x'의 binary 값을 비교한 cross-entropy(Reconstruction loss)
+    KLD: encoder를 통해 근사된 q(z)와 실제분포 p(z) 사이의 KL-divergence
     """
     log_var = std.pow(2).log()
     BCE = F.binary_cross_entropy(recon_x, x, reduction="mean") * mu.size(0)
