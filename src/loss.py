15be289fa830e0c0460c0994167a6a4fa1c6acee
jihoon <jh921204@naver.com>
2023-06-13 15:51:22 +0900

    feat. add KL_annealing function

--- src/loss.py
+++ src/loss.py
@@ -2,15 +2,18 @@ import torch
 import torch.nn.functional as F
 
 
-def ELBO_loss(recon_x, x, mu, std):
+def ELBO_loss(recon_x, x, mu, std, epoch):
     """
     BCE: 입력값 x와 decoder를 거쳐 생성된 x'의 binary 값을 비교한 cross-entropy(Reconstruction loss)
     KLD: encoder를 통해 근사된 q(z)와 실제분포 p(z) 사이의 KL-divergence
     """
     log_var = std.pow(2).log()
-    BCE = F.binary_cross_entropy(recon_x, x, reduction="mean") * mu.size(0)
-    KLD = (-0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())) / (mu.size(0) * mu.size(1))
-    # print(BCE)
-    # print(KLD)
+    beta = KL_annealing(kl_weight=1.0, kl_weight_start=0.5, rate=0.99995, epoch=epoch)
+    BCE = F.binary_cross_entropy(recon_x, x, reduction="sum")
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
 
-    return BCE + KLD
+    return (BCE + (beta * KLD)) / recon_x.shape[0]
+
+
+def KL_annealing(kl_weight, kl_weight_start, rate=0.99995, epoch=0):
+    return kl_weight - (kl_weight - kl_weight_start) * (rate) ** epoch
